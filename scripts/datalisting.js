4df22f19c2ca248390b229eddf44b9b8be44346b
Ahmad Ismail <ahmedeldeehy30305@gmail.com>
2024-04-19 11:03:35 +0200

    File Syncing, Again

--- scripts/datalisting.js
+++ scripts/datalisting.js
@@ -1,41 +1,40 @@
-document.addEventListener('DOMContentLoaded', function () {
-  displayData();
+document.addEventListener("DOMContentLoaded", function () {
+	displayData();
 });
 
 function displayData() {
-  const dataList = document.getElementById('dataList');
-  const applications = JSON.parse(localStorage.getItem('jobApplications')) || [];
-  const posts = JSON.parse(localStorage.getItem('postedJobs')) || [];
+	const dataList = document.getElementById("dataList");
+	const applications =
+		JSON.parse(localStorage.getItem("jobApplications")) || [];
+	const posts = JSON.parse(localStorage.getItem("postedJobs")) || [];
 
-  if (applications.length == 0 && posts.length == 0) {
-    dataList.innerHTML = "<li>No applications and jobs found</li>";
-    return;
-  }
+	if (applications.length == 0 && posts.length == 0) {
+		dataList.innerHTML = "<li>No applications and jobs found</li>";
+		return;
+	}
 
+	dataList.innerHTML = ""; // Clear previous entries
 
-
-  dataList.innerHTML = ''; // Clear previous entries
-
-  applications.forEach(
-    function (application, index) {
-      const listItem = document.createElement('li');
-      listItem.innerHTML = `
+	applications.forEach(function (application, index) {
+		const listItem = document.createElement("li");
+		listItem.innerHTML = `
       <strong>Application ${index + 1}</strong><br>
       Full Name: ${application.fullname}<br>
       Email: ${application.email}<br>
       Phone: ${application.phone}<br>
       Education: ${application.education}<br>
       Experience: ${application.experience}<br>
-      LinkedIn: <a href="${application.linkedin}">${application.linkedin}</a><br>
+      LinkedIn: <a href="${application.linkedin}">${
+			application.linkedin
+		}</a><br>
       Comments: ${application.comments}<br>
       <hr>
     `;
-      dataList.appendChild(listItem);
-    })
-  posts.forEach(
-    function (post, index) {
-      const listItem = document.createElement('li');
-      listItem.innerHTML = `
+		dataList.appendChild(listItem);
+	});
+	posts.forEach(function (post, index) {
+		const listItem = document.createElement("li");
+		listItem.innerHTML = `
       <strong>Job ${index + 1}</strong><br>
       Job Title: ${post.title}<br>
       Job Description: ${post.description}<br>
@@ -46,7 +45,6 @@ function displayData() {
       Company link: <a href="${post.companyLink}">${post.companyLink}</a><br>
       <hr>
     `;
-      dataList.appendChild(listItem);
-    })
-    ;
+		dataList.appendChild(listItem);
+	});
 }
--- scripts/jobpost.js
+++ scripts/jobpost.js
@@ -1,32 +1,31 @@
 function savePost(event) {
-  event.preventDefault();
+	event.preventDefault();
 
-  // Get form data
-  const postFormData = {
-    title: document.getElementById('title').value,
-    description: document.getElementById('description').value,
-    location: document.getElementById('location').value,
-    requirements: document.getElementById('requirements').value,
-    salary: document.getElementById('salary').value,
-    overview: document.getElementById('overview').value,
-    companyLink: document.getElementById('companyLink').value,
-  };
+	// Get form data
+	const postFormData = {
+		title: document.getElementById("title").value,
+		description: document.getElementById("description").value,
+		location: document.getElementById("location").value,
+		requirements: document.getElementById("requirements").value,
+		salary: document.getElementById("salary").value,
+		overview: document.getElementById("overview").value,
+		companyLink: document.getElementById("companyLink").value,
+	};
 
-  // Save form data to local storage
-  let posts = new Array();
-  posts = JSON.parse(localStorage.getItem('postedJobs')) || [];
-  posts.push(postFormData);
-  localStorage.setItem('postedJobs', JSON.stringify(posts));
+	// Save form data to local storage
+	let posts = new Array();
+	posts = JSON.parse(localStorage.getItem("postedJobs")) || [];
+	posts.push(postFormData);
+	localStorage.setItem("postedJobs", JSON.stringify(posts));
 
+	//Clear form fields after submission
+	document.getElementById("title").value = "";
+	document.getElementById("description").value = "";
+	document.getElementById("location").value = "";
+	document.getElementById("requirements").value = "";
+	document.getElementById("salary").value = "";
+	document.getElementById("overview").value = "";
+	document.getElementById("companyLink").value = "";
 
-  //Clear form fields after submission
-  document.getElementById('title').value = '';
-  document.getElementById('description').value = '';
-  document.getElementById('location').value = '';
-  document.getElementById('requirements').value = '';
-  document.getElementById('salary').value = '';
-  document.getElementById('overview').value = '';
-  document.getElementById('companyLink').value = '';
-
-  alert('Job posted successfully!');
+	alert("Job posted successfully!");
 }
