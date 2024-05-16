01caa394ffd53d780fce0bc1fb0e6654ab2062f9
Ahmad Ismail <ahmedeldeehy30305@gmail.com>
2024-05-16 22:03:43 +0300

    HTML Templates Added + File Paths Fixed

--- jobquest_project/jobquest/jobquest/urls.py
+++ jobquest_project/jobquest/jobquest/urls.py
@@ -17,7 +17,9 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
+from website import views
 
 urlpatterns = [
+    path('jobquest/', views.website, name='jobquests'),
     path("admin/", admin.site.urls),
 ]
