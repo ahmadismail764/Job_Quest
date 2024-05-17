252c8bf25ec8d17e9b6746bb0d368f4bb7ddeece
abdoahmed100 <112801890+abdoahmed100@users.noreply.github.com>
2024-05-17 03:59:49 +0300

    semi final work

--- JobQuest/project/user/urls.py
+++ JobQuest/project/user/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.home, name='home'),
-    path('signup', views.signup, name='signup'),
-    path('login', views.login, name='login'),
-    path('admindashboard', views.admindashboard, name='admindashboard'),
-    path('userdashboard', views.userdashboard, name='userdashboard')
+    path('signup/', views.signup, name='signup'),
+    path('login/', views.login, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('admindashboard/', views.admindashboard, name='admindashboard'),
+    path('userdashboard/', views.userdashboard, name='userdashboard')
 ]
--- JobQuest/project/user/views.py
+++ JobQuest/project/user/views.py
@@ -1,22 +1,78 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
+from django.contrib.auth import logout as auth_logout
+from .forms import UserRegistrationForm
+from .forms import LoginForm
+from django.contrib.auth.decorators import login_required
+
+def login(request):
+    error_message = None 
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                # login(request, user)
+                if user.is_staff:
+                    return redirect('admindashboard')
+                else:
+                    return redirect('userdashboard')  
+            else:
+                error_message = "Invalid username or password. Please try again."
+    else:
+        form = LoginForm()
+    
+    return render(request, 'login.html', {'form': form, 'error_message': error_message})
+
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            user_type = form.cleaned_data['type_job']
+            company_name = form.cleaned_data.get('company_name')
+            
+            # Check if the username already exists
+            if User.objects.filter(username=username).exists():
+                return render(request, 'signup.html', {'form': form, 'error_message': 'Username already exists. Please choose a different username.'})
+            
+            if user_type == 'admin' and not company_name:
+                return render(request, 'signup.html', {'form': form, 'error_message': 'Company Name is required for Company Admin.'})
+            
+            user = User.objects.create_user(username=username, email=email, password=password)
+            if user_type == 'admin':
+                # Create Company Admin user
+                user.is_staff = True
+                user.save()
+            
+            # login(request, user)
+            return redirect('login')  # Redirect to the login URL
+            
+    else:
+        form = UserRegistrationForm()
+    
+    return render(request, 'signup.html', {'form': form})
+
+
+def logout_view(request):
+    auth_logout(request)
+    return redirect('home')
+
 
 def home(request):
     return render(request , 'homepage.html')
 
-def signup(request):
-    return render(request , 'signup.html')
-
-def login(request):
-    return render(request , 'login.html')
-
+# @login_required(login_url='login')
 def admindashboard(request):
     return render(request , 'admindashboard.html')
-
+# @login_required(login_url='login')
 def userdashboard(request):
     return render(request , 'userdashboard.html')
 
-def admindashboard(request):
-    return render(request , 'admindashboard.html')
-
-def userdashboard(request):
-    return render(request , 'userdashboard.html')
