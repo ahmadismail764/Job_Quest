faa083aba1845f879cbdd7e9fd6b365a2cc6c429
Mohammed Zeinhom <mohamedzenhom200@gmail.com>
2024-05-20 20:46:37 +0300

    project added

---
+++ JobQuest/project/user/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class UserAccount(models.Model):
+    email = models.EmailField()
+    password = models.CharField(max_length=128)
+    username = models.CharField(max_length=150)
+    user_type = models.CharField(max_length=50, choices=[('user', 'User'), ('admin', 'Admin')])
+    company_name = models.CharField(max_length=150, blank=True, null=True)
+
+    def __str__(self):
+        return self.email
