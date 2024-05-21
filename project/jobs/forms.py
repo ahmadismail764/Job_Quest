8aa4014c0b43fc38fe6247ddacaaa23c8cea6b0a
Mohammed Zeinhom <mohamedzenhom200@gmail.com>
2024-05-21 23:37:20 +0300

    الحقونا بجد

--- project/jobs/forms.py
+++ project/jobs/forms.py
@@ -1,15 +1,12 @@
 from django import forms
-from .models import Application
+from .models import Job, Application
 
-class ApplicationForm(forms.Form):
-    full_name = forms.CharField(max_length=100)
-    email = forms.EmailField()
-    phone = forms.CharField(max_length=20)
-    education = forms.ChoiceField(choices=(('high_school', 'High School'), ('bachelor', 'Bachelor'), ('master', 'Master'), ('doctorate', 'Doctorate')))
-    experience = forms.IntegerField()
-    # resume = forms.FileField(upload_to='resumes/%Y/%m/%d/')
-    # cover_letter = forms.FileField(upload_to='cover_letters/%Y/%m/%d/')
-    linkedin = forms.URLField()
-    comments = forms.CharField(widget=forms.Textarea)
-    # applied_by = forms.CharField(max_length=100)
-    # created_at = forms.DateTimeField(auto_now_add=True)
+# class JobForm(forms.ModelForm):
+#     class Meta:
+#         model = Job
+#         fields = ['title', 'description']
+
+class ApplicationForm(forms.ModelForm):
+    class Meta:
+        model = Application
+        fields = ['full_name', 'email', 'phone', 'education', 'experience', 'linkedin', 'comments']
