82682cce57ebeb9ce4dd453bb0955949ee865eeb
ahmadismail764 <ahmedeldeehy30305@gmail.com>
2024-05-22 17:56:56 +0300

    Slight Change, nvm

--- project/user/models.py
+++ project/user/models.py
@@ -60,7 +60,6 @@ class Company(models.Model):
 
 
 class Award(models.Model):
-    company = models.ForeignKey(Company, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     provider = models.CharField(max_length=50)
     date = models.CharField(max_length=100)
