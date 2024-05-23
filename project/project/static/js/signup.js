b4c80b18e20d937e108be3fd6bd2f91291a5c59d
Mohammed Zeinhom <mohamedzenhom200@gmail.com>
2024-05-23 22:12:28 +0300

    apply js on sign up

--- project/project/static/js/signup.js
+++ project/project/static/js/signup.js
@@ -1,71 +1,23 @@
-function checkCompanyNameInput() {
-	var userType = document.querySelector('input[name="type_job"]:checked').value;
-	var companyNameDiv = document.getElementById("companyNameDiv");
+document.addEventListener('DOMContentLoaded', function () {
+    const userTypeRadios = document.querySelectorAll('input[name="type_job"]');
+    const companyNameField = document.querySelector('#id_company_name');
+    const companyLabel = document.querySelector('#companyLabel');
 
-	if (userType === "admin") {
-		companyNameDiv.style.display = "block";
-	} else {
-		companyNameDiv.style.display = "none";
-	}
-}
+    function toggleCompanyNameField() {
+        const selectedUserType = document.querySelector('input[name="type_job"]:checked').value;
+        if (selectedUserType === 'admin') {
+            companyNameField.style.display = 'block';
+            companyLabel.style.display = 'block';
+        } else {
+            companyNameField.style.display = 'none';
+            companyLabel.style.display = 'none';
+        }
+    }
 
-function signUp() {
-	var username = document.getElementById("username").value;
-	var password = document.getElementById("password").value;
-	var confirmPassword = document.getElementById("confirm_password").value;
-	var email = document.getElementById("email").value;
-	var userType = document.querySelector('input[name="type_job"]:checked').value;
-	var companyName = "";
-	var postedJobs = new Array();
-	var applliedJobs = new Array();
+    userTypeRadios.forEach(radio => {
+        radio.addEventListener('change', toggleCompanyNameField);
+    });
 
-	// check confirming password
-	if (password !== confirmPassword) {
-		alert("Passwords do not match");
-		return false; 
-	}
-
-	// check validation of email must have (.) and one of ['net', 'org', 'edu', 'gov', 'com']
-	if (email.indexOf(".") === -1) {
-		alert("Invalid email address please enter a valid email.");
-		return false; 
-	}
-
-	var domain = email.split(".")[1];
-	var validDomains = ["net", "org", "edu", "gov", "com"];
-	if (validDomains.indexOf(domain) == -1) {
-		alert("Invalid email address. Please enter a valid email.");
-		return false; 
-	}
-
-	var users = JSON.parse(localStorage.getItem("users")) || [];
-	var existingUser = users.find(function (user) {
-		return user.email === email;
-	});
-
-	if (existingUser) {
-		alert("Email already exists");
-		return false; 
-	}
-
-	if (userType === "admin") {
-		companyName = document.getElementById("company_name").value;
-	}
-
-	var user = {
-		username: username,
-		password: password,
-		email: email,
-		userType: userType,
-		companyName: companyName,
-		postedJobs: postedJobs,
-		applliedJobs: applliedJobs,
-	};
-
-	users = JSON.parse(localStorage.getItem("users")) || [];
-	users.push(user);
-	localStorage.setItem("users", JSON.stringify(users));
-	alert("Sign Up Successful");
-	window.location.href = "login.html";
-	return true;
-}
+    // Initial check
+    toggleCompanyNameField();
+});
