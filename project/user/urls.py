053d37fb1c302ba29a368db901e07f1d654eb5d1
ahmadismail764 <ahmedeldeehy30305@gmail.com>
2024-06-27 09:54:16 +0300

    A few stuff

--- project/user/urls.py
+++ project/user/urls.py
@@ -10,5 +10,10 @@ urlpatterns = [
     path('logout/', views.logout_view, name='logout'),
     path('admindashboard/', views.admindashboard, name='admindashboard'),
     path('userdashboard/', views.userdashboard, name='userdashboard'),
+    path('add_award.html/', views.add_award, name='add-award'),
+    path('add_cert.html/', views.add_cert, name='add-cert'),
+    path('add_exper.html/', views.add_exper, name='add-exper'),
+    path('add_license.html/', views.add_license, name='add-license'),
+    path('add_project.html/', views.add_project, name='add-project'),
     path('delete/<int:job_id>/', views.delete_job, name='delete-job'),
 ]
